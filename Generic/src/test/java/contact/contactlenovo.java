95921b804ecaf18fd69571d14aff77a18dd78746
fazimi1 <fazliazimi@gmail.com>
2019-03-17 22:59:20 -0400

    update

--- Generic/src/test/java/contact/contactlenovo.java
+++ Generic/src/test/java/contact/contactlenovo.java
@@ -2,14 +2,25 @@ package contact;
 
 import base.CommonAPI;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class contactlenovo extends CommonAPI {
     @Test
     public void search() {
         driver.findElement(By.id("contact")).click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-}
+
+        driver.findElement(By.linkText("CHAT Available 24/7")).click();
+
+
+        driver.findElement(By.id("pFirstName")).sendKeys("Hi");
+
+
+            }
+        }
+
+
+
